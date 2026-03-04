d0b6a1d7db36306a8e1f8e036d54804e7661eb42
yang_jieun <yjieun241@gmail.com>
2026-03-04 15:01:49 +0900

    anove02

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import store from './components/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/ANOVE'>
       <App />
     </BrowserRouter>
   </Provider>
